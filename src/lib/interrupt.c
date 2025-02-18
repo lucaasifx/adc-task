37c7f8c8659a32328c3632cdf781d97d7d0d8ad6
Lucas Emanoel <lucaasifx@gmail.com>
2025-02-18 18:00:10 -0300

    :sparkles: feat: Botão A agora desativa o PWM dos leds

--- src/lib/interrupt.c
+++ src/lib/interrupt.c
@@ -4,7 +4,11 @@
 #include "pico/bootrom.h"
 #include "pico/stdlib.h"
 #include "ssd1306.h"
+#include "hardware/pwm.h"
 #include <stdio.h>
+
+
+bool PWM = true;
 
 // Função que lida com eventos de interrupção
 void gpio_irq_handler(uint gpio, uint32_t events) {
@@ -17,7 +21,17 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
         }
 
         if(gpio == BUTTON_A) {
-            
+            uint red_slice = pwm_gpio_to_slice_num(LED_RED);
+            uint blue_slice = pwm_gpio_to_slice_num(LED_BLUE);
+            if(PWM) {
+                pwm_set_enabled(red_slice, false);
+                pwm_set_enabled(blue_slice, false);
+            }
+            else {
+                pwm_set_enabled(red_slice, true);
+                pwm_set_enabled(blue_slice, true);
+            }
+            PWM = !PWM;
         }
 
         if(gpio == BUTTON_B) {
